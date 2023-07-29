d82e1ebb2550fc490e640511791a7407245b1893
emirom <meyari73@gmail.com>
2023-07-29 20:44:40 +0330

    removed mongo getCountryProvinces as temp query

--- src/database/mongo/pure/services/country/getCountryProvinces.service.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from "mongodb";
-import { collections } from "../..";
-
-
-const getCountryProvincesService = async (countryId : ObjectId) => {
-	return await collections.provinces!.find({countryId:countryId}).toArray();
-}
-
-export default getCountryProvincesService
