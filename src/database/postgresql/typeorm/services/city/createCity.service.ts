00cf2bac79b7251c0611c88b9d02a4d7fc315bca
emirom <meyari73@gmail.com>
2023-08-07 12:17:01 +0330

    removed typeorm copied services till typeorm issue fix

--- src/database/postgresql/typeorm/services/city/createCity.service.ts
+++
@@ -1,9 +0,0 @@
-import { collections } from "../..";
-import City from "../../models/City";
-
-
-const createCityService = async (CityInput : City) => {
-	return await collections.cities!.insertOne(CityInput);	
-}
-
-export default createCityService
