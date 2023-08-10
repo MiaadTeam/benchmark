4cb1dc9f5ebc2ebe9b9e82ed1f8acc0a4d188606
emirom <meyari73@gmail.com>
2023-08-10 12:20:59 +0330

    put bootsrap in trycatch

--- src/server/express.ts
+++ src/server/express.ts
@@ -10,42 +10,42 @@ import errorMiddleware from "../middleware/error.middleware";
 import routes from "../routes";
 
 const ServerPort = process.env.SERVER_PORT || 9900;
-
-const app = express();
-app.use(express.json());
-app.use(errorMiddleware)
-
-routes(app);
-
-(async () => {
-  process.argv.map(async (val, _index, array) => {
-    if (val ==="--mongoose") {
-      await connectToMongoose();
-      if (array[3] === "--seed") {
-        await seedMongoose()
+try {
+  (async () => {
+    process.argv.map(async (val, _index, array) => {
+      if (val ==="--mongoose") {
+        await connectToMongoose();
+        if (array[3] === "--seed") {
+          await seedMongoose()
+        }
+        
+      } else if (val ==="--mongo") {
+        await connectToMongoDB();
+        if (array[3] === "--seed") {
+          await seedMongoDB()
+        }
+      } else if (val ==="--typeorm") {
+          createTypeormConnection();
+        if (array[3] === "--seed") {
+          // await seedTypeOrm()
+        }
+      } else if (val ==="--prisma") {
+          await createPrismaConnection();
+        if (array[3] === "--seed") {
+          // await seedPrisma()
+        }
       }
-      
-    } else if (val ==="--mongo") {
-      await connectToMongoDB();
-      if (array[3] === "--seed") {
-        await seedMongoDB()
-      }
-    } else if (val ==="--typeorm") {
-        createTypeormConnection();
-      if (array[3] === "--seed") {
-        // await seedTypeOrm()
-      }
-    } else if (val ==="--prisma") {
-        await createPrismaConnection();
-      if (array[3] === "--seed") {
-        // await seedPrisma()
-      }
-  }
-
+    })
+  
+    const app = express();
+    app.use(express.json());
+    app.use(errorMiddleware)
+    routes(app);
     
-  })
-
-  app.listen(ServerPort, () => {
-    console.log(`Express server is up at http://localhost:${ServerPort}`);
-  });
-})();
+    app.listen(ServerPort, () => {
+      console.log(`Express server is up at http://localhost:${ServerPort}`);
+    });
+  })();
+} catch (error) {
+  console.log( "bootstrap error :" + error)
+}
