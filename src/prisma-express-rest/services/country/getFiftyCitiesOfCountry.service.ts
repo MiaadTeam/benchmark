cdabab3e74f8a5bfc45e15e56554cf0cafcd1c81
emirom <meyari73@gmail.com>
2023-10-07 16:49:48 +0330

    fix( prisma ): use findMany for getFiftyCities

--- src/prisma-express-rest/services/country/getFiftyCitiesOfCountry.service.ts
+++ src/prisma-express-rest/services/country/getFiftyCitiesOfCountry.service.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 const getFiftyCitiesOfCountryService = async (limit = 50, pageNumber = 1) => {
-	const countries=	await prisma.country.findMany({
+	const countries= await prisma.country.findMany({
 		select: {
 			name: true,
 			abb: true,
@@ -32,7 +32,6 @@ const getFiftyCitiesOfCountryService = async (limit = 50, pageNumber = 1) => {
 		}
 	
 	})
-	console.log('============= countries ============ : ', countries);
 	return countries
 }
 
