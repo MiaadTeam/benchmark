648b2a603096bfad2f0d50fcd29aa82284629aac
emirom <meyari73@gmail.com>
2023-10-07 19:15:38 +0330

    add init migration of prisma-graphql

--- src/prisma-express-graphql/services/city/updateCity.service.ts
+++ src/prisma-express-graphql/services/city/updateCity.service.ts
@@ -1,8 +1,7 @@
 import { ICityDTO } from "../../dto/location.dto";
-import City from "../../repository/city";
 
 const updateCityService = async ( _id:number, updatedCity : Partial<ICityDTO> ) => {
-	return await City.update(_id, updatedCity);
+	return await .update(_id, updatedCity);
 }
 
 export default updateCityService
