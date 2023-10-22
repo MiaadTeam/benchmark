8339bc68d881778783d8c18237d70e89b968e65c
emirom <meyari73@gmail.com>
2023-10-22 20:25:33 +0330

    fix: moved deno-lesan to src and test results to rout directory

--- src/typeorm-express-rest/services/country/createCountry.service.ts
+++
@@ -1,9 +0,0 @@
-import { Country } from "../../models/Country";
-
-
-const createCountryService = async (countryInput : Country) => {
-	const country = Country.create(countryInput);	
-	return await country.save()
-}
-
-export default createCountryService
