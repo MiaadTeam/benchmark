7a4d38dca75a82a8125fec22b424b6a0bc1e51e4
emirom <meyari73@gmail.com>
2023-10-23 15:40:11 +0330

    fix (mongo): getFiftyCitiesOfCountry routing

--- src/mongo-express-rest/routes/mongo.routes.ts
+++ src/mongo-express-rest/routes/mongo.routes.ts
@@ -7,7 +7,7 @@ const router = Router()
  *  also the crud of all entities are available in controller folder
  *  we add bench mark routes
  *  */
-router.get( "/fiftyCitiesOfFiftyProvinces", getFiftyCitiesOfCountry )
+router.get( "/getFiftyCitiesOfCountry", getFiftyCitiesOfCountry )
 router.post("/seed", seedController )
 
 export default router
