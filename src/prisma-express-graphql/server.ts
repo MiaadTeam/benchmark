2e74a79dd5ad21098d55ff702b03752c536306b5
emirom <meyari73@gmail.com>
2023-12-02 10:23:17 +0330

    fix: add cores

--- src/prisma-express-graphql/server.ts
+++ src/prisma-express-graphql/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { createPrismaConnection } from "./database/connection";
@@ -25,6 +26,7 @@ try {
       })
     );
     app.use(express.json());
+    app.use(cors)
     
     app.listen(SERVER_PORT, () => {
       console.log(`Express server ( prisma + graphql ) is up at http://localhost:${SERVER_PORT}/graphql`);
